a236d47456af0e23c5368d6dec4a71aa5bba50f1
Nave066 <jayamnaveen68@gmail.com>
2022-01-31 18:41:39 +0530

    The Daily Limit Defined UC1

---
+++ DailyLimit.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamblingSimulator
+{
+    internal class UseCase1
+    {
+        public static void UC1_GamblingDailyLimit()
+        {
+            Console.WriteLine("Welcome to Gambling");
+            int DailyMoneyLimit = 100;
+            int DailyBetLimit = 1;
+        }
+    }
+}
